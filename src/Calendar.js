46d23f72fdf7bbda7c4c01a8b4ed034c49c1e053
Lorenzo DeSimone <lorenzodesimone149@gmail.com>
2019-01-19 16:07:41 -0500

    linked events

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,6 +1,53 @@
 import React, { Component } from 'react';
+import firebase, { auth, provider } from './Firestore.js';
+import './Firestore.js';
+import { Link } from "react-router-dom";
+
 
 class Calendar extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      name: '',
+      eventsFeedData: []
+    }
+  }
+
+  componentDidMount(){
+    let currentComponent = this;
+    //Load Firebase Stuff
+    const firestore = firebase.firestore();
+    const settings = {timestampsInSnapshots: false};
+    firestore.settings(settings);
+
+    //Load Location or Path for Query
+    const db = firebase.firestore();
+    const compRef = db.collection("events");
+
+    var ccData = []
+    //Query Data
+    compRef.get().then(function(querySnapshot) {
+
+      querySnapshot.forEach(function(doc) {
+
+
+          let newProg = { 
+            "attending": doc.data().attending,
+            "description": doc.data().description, 
+            "location": doc.data().image,
+            "name": doc.data().name,
+            "image": doc.data().image,
+            "uid": doc.id,
+          };
+          ccData.push(newProg)
+      });
+      currentComponent.setState({ eventsFeedData: ccData });    
+
+    });    
+
+  }
+  
     render() {
       return (
       <div className="mainContent">
@@ -11,7 +58,18 @@ class Calendar extends Component {
               <br></br>
               <h2>Events</h2>
               <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
-                          
+              <hr></hr> 
+
+              {this.state.eventsFeedData.map(function(event, idx){
+                return (
+                        
+                  <Link to={'/events/'+event.uid}>
+                    <div> {event.name}</div>
+                  </Link>    
+                );
+             })}  
+
+
             </div>
             <div className="col-sm-1"></div>
           </div>
