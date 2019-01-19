bb080e2a1a86980f45fd0749a1240a99c13cf31b
Michael Osei <mike3osei@gmail.com>
2019-01-19 17:52:50 -0500

    add fix location events

--- src/Locations.js
+++ src/Locations.js
@@ -19,6 +19,52 @@ class Locations extends Component {
       if (window.navigator.geolocation) {
         // geolocation is available
         console.log("available")
+        window.navigator.geolocation.getCurrentPosition(
+
+          // Success callback
+          function(position) {
+      
+              /*
+              position is an object containing various information about
+              the acquired device location:
+      
+              position = {
+                  coords: {
+                      latitude - Geographical latitude in decimal degrees.
+                      longitude - Geographical longitude in decimal degrees. 
+                      altitude - Height in meters relative to sea level.
+                      accuracy - Possible error margin for the coordinates in meters. 
+                      altitudeAccuracy - Possible error margin for the altitude in meters. 
+                      heading - The direction of the device in degrees relative to north. 
+                      speed - The velocity of the device in meters per second.
+                  }
+                  timestamp - The time at which the location was retrieved.
+              }
+              */
+
+              console.log(position)
+      
+          },
+      
+          // Optional error callback
+          function(error){
+      
+              /* 
+              In the error object is stored the reason for the failed attempt:
+      
+              error = {
+                  code - Error code representing the type of error 
+                          1 - PERMISSION_DENIED
+                          2 - POSITION_UNAVAILABLE
+                          3 - TIMEOUT
+      
+                  message - Details about the error in human-readable format.
+              }
+              */
+      
+          }
+      );
+
       } 
       else {
         // geolocation is not supported
