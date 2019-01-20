f69f3d4be71ce77d61f6b3ef94b0b5a2c8f2c575
Robear Mankaryous <rmankaryous12@gmail.com>
2019-01-19 19:40:16 -0500

    search bar

--- src/my_great_place.jsx
+++ src/my_great_place.jsx
@@ -1,84 +1,63 @@
 import React, {PropTypes, Component} from 'react';
 import shouldPureComponentUpdate from 'react-pure-render/function';
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 export default class Annotation extends Component {
 
-  constructor() {
-      super();
-      this.annoAction = this.annoAction.bind(this);
-  }    
+    constructor() {
+        super();
+        this.annoAction = this.annoAction.bind(this);
+    }
 
-  static propTypes = {
-    text: ""
-  };
+    static propTypes = {
+        text: ""
+    };
 
-  state = {
-    showResults: false,
-  };
+    state = {
+        showResults: false,
+    };
 
-  annoAction(index, e, post) {  
+    annoAction(index, e, post) {
 
-    post.showing = !post.showing
+        post.showing = !post.showing
 
-    console.log("hi");
-  }
+        console.log("hi");
+    }
 
-  static defaultProps = {};
+    static defaultProps = {};
 
-  shouldComponentUpdate = shouldPureComponentUpdate;
+    shouldComponentUpdate = shouldPureComponentUpdate;
 
-  render() {
-    return (
-       <div className={"waypoint"} background-image={this.props.tyProImg}>
- 
-          <div>
-
-          <Link  to={{pathname: '/location/' + this.props.text}}>
-
-            <i className="material-icons sideButton">{this.props.tyImg}</i>
-            </Link>
-            <div className="imageDiaOne">
-             <img src={this.props.tyProImg}></img>
-             <div className="overlayIm"></div>
-             <i className="material-icons centerIconGrad">{this.props.tyImg}</i>
-             
-
-             <div class="overlay">
-                <div className="hoverDialog">
-                    <Link className="labelHall" to={{pathname: '/location/' + this.props.text}}>
-                        <div className="profileImage"><img src={this.props.tyProImg}></img></div>
-                        <h3 className="placeName">{this.props.text}</h3>
-                    </Link>
+    render() {
+        return (
+            <div className={"waypoint"}>
+                <img className={"waypoint-image"} src={this.props.tyProImg}/>
+                <div className="overlay">
+                    <div className="hoverDialog">
+                        <Link className="labelHall" to={{pathname: '/location/' + this.props.text}}>
+                            <div className="profileImage"><img src={this.props.tyProImg}/></div>
+                            <h3 className="placeName">{this.props.text}</h3>
+                        </Link>
+                    </div>
                 </div>
+
+
+                {this.state.showResults ?
+
+                    <div className="hoverDialog">
+                        <Link className="labelHall" to={{pathname: '/location/' + this.props.text}}>
+                            <div className="profileImage"><img src={this.props.tyProImg}></img></div>
+                            <h3 className="placeName">{this.props.text}</h3>
+                        </Link>
+                    </div>
+                    :
+
+                    <div></div>
+
+                }
+
+
             </div>
-            </div>
-
-
-            <div className="imageDia">
-             <img src={this.props.tyProImg}></img>
-            </div>
-
-          </div>
-
-
-
-          { this.state.showResults ? 
-          
-                <div className="hoverDialog">
-                    <Link className="labelHall" to={{pathname: '/location/' + this.props.text}}>
-                        <div className="profileImage"><img src={this.props.tyProImg}></img></div>
-                        <h3 className="placeName">{this.props.text}</h3>
-                    </Link>
-                </div>
-          : 
-          
-              <div></div>
-          
-          }
-
- 
-       </div>
-    ); 
-  }
+        );
+    }
 }
